15b6139c881633594729875960643793441e30aa
Code Monkey King <freesoft.for.people@gmail.com>
2023-09-19 11:20:50 +0300

    added timezone

--- root/etc/shell.sh
+++ root/etc/shell.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-PATH=$PATH:/sbin:/usr/local/bin LD_LIBRARY_PATH=/lib:/usr/local/lib sh
+PATH=$PATH:/sbin:/usr/local/bin LD_LIBRARY_PATH=/lib:/usr/local/lib TZ=EET-2EEST,M3.5.0/3,M10.5.0/4 sh
