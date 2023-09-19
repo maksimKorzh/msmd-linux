f62086d60c8107472f44cf1ef30829f9658a07d2
Code Monkey King <freesoft.for.people@gmail.com>
2023-09-19 14:13:21 +0300

    added tce-load package manager

--- root/etc/shell.sh
+++ root/etc/shell.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-PATH=$PATH:/sbin:/usr/local/bin LD_LIBRARY_PATH=/lib:/usr/local/lib TZ=EET-2EEST,M3.5.0/3,M10.5.0/4 sh
+PATH=$PATH:/sbin:/usr/local/bin:/usr/local/sbin LD_LIBRARY_PATH=/lib:/usr/local/lib TZ=EET-2EEST,M3.5.0/3,M10.5.0/4 sh
