2113974f5215b05fe83a9a250dd40ef1e28c1584
Code Monkey King <freesoft.for.people@gmail.com>
2023-10-03 07:34:05 +0300

    updated X files

--- X/install_x.sh
+++ X/install_x.sh
@@ -24,12 +24,16 @@ sudo mkdir -p /etc/init.d
 sudo mkdir -p /etc/sysconfig
 sudo chmod -R +w /etc/sysconfig
 sudo echo "Xorg" | sudo tee /etc/sysconfig/Xserver
-sudo echo "jwm" | sudo tee /etc/sysconfig/desktop
+sudo echo "flwm" | sudo tee /etc/sysconfig/desktop
 sudo echo "msmd" | sudo tee /etc/sysconfig/tcuser
 sudo echo "wbar" | sudo tee /etc/sysconfig/icons
 
 # Add missing scripts & files
-git clone https://github.com/tinycorelinux/Core-scripts
-sudo cp Core-scripts/etc/init.d/tc-functions /etc/init.d/tc-functions
-sudo cp Core-scripts/usr/bin/select /usr/bin/select
-sudo cp XSESSION /etc/skel/.xsession
+#git clone https://github.com/tinycorelinux/Core-scripts
+#sudo cp Core-scripts/etc/init.d/tc-functions /etc/init.d/tc-functions
+#sudo cp Core-scripts/usr/bin/select /usr/bin/select
+sudo cp XSESSION /home/msmd/.xsession
+cp WBARCONF /home/msmd/.wbarconf
+cp XDEFAULTS /home/msmd/.Xdefaults
+cp SETBACKGROUND /home/msmd/.setbackground
+ln -s /usr/local/tce.icons /home/msmd/.wbar
