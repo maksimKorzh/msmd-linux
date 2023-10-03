f02f29050224aa1ea604649adadc6651b399b7ad
Code Monkey King <freesoft.for.people@gmail.com>
2023-10-03 04:14:43 +0300

    updated install_x.sh

--- X/install_x.sh
+++ X/install_x.sh
@@ -10,6 +10,10 @@
 dipi Xfbdev
 dipi jwm
 
+# Fix permissions
+sed -i 's/staff/msmd/g' /usr/local/bin/*
+
+# Provide Tiny Core related configurations
 sudo mkdir -p /etc/init.d
 sudo mkdir -p /etc/sysconfig
 echo "Xfbdev" > /etc/sysconfig/Xserver
@@ -17,6 +21,7 @@ echo "jwm" > /etc/sysconfig/desktop
 echo "msmd" > /etc/sysconfig/tcuser
 echo "wbar" > /etc/sysconfig/icons
 
+# Add missing scripts
 git clone https://github.com/tinycorelinux/Core-scripts
 sudo cp Core-scripts/etc/init.d/tc-functions /etc/init.d/tc-functions
 sudo cp Core-scripts/usr/bin/select /usr/bin/select
