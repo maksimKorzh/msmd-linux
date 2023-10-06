d353fdfc84b8319ce198270af16bdb4a7416e827
Code Monkey King <freesoft.for.people@gmail.com>
2023-10-06 07:03:14 +0300

    added jwm config files

--- X/install_x.sh
+++ X/install_x.sh
@@ -7,15 +7,15 @@
 ########################################
 
 # Install X packages
-for PAC in $(cat packages.lst); do
-  dipi $PAC
-done
+#for PAC in $(cat packages.lst); do
+#  dipi $PAC
+#done
 
 # Fix permissions
-sudo sed -i 's/staff/msmd/g' /usr/local/bin/*
+#sudo sed -i 's/staff/msmd/g' /usr/local/bin/*
 
 # Symlink libs
-sudo ln -s /usr/local/lib/* /lib/
+#sudo ln -s /usr/local/lib/* /lib/
 
 # Provide Tiny Core related configurations
 sudo mkdir -p /var/log
@@ -28,14 +28,15 @@ sudo echo "jwm" | sudo tee /etc/sysconfig/desktop
 sudo echo "msmd" | sudo tee /etc/sysconfig/tcuser
 
 # Add missing scripts & files
-git clone https://github.com/tinycorelinux/Core-scripts
-sudo cp Core-scripts/etc/init.d/tc-functions /etc/init.d/tc-functions
-sudo cp Core-scripts/usr/bin/select /usr/bin/select
+#git clone https://github.com/tinycorelinux/Core-scripts
+#sudo cp Core-scripts/etc/init.d/tc-functions /etc/init.d/tc-functions
+#sudo cp Core-scripts/usr/bin/select /usr/bin/select
 sudo cp XSESSION /etc/skel/.xsession
 cp SETBACKGROUND /home/msmd/.setbackground
+cp .jwm* /home/msmd/
 
 # Setup X environment
 xsetup.sh
 sed -i '1s/^/sudo /' /home/msmd/.xsession
-sudo mkdir -p /dev/pts
-sudo mount -t devpts devpts /dev/pts
+echo 'sudo mkdir -p /dev/pts' >> /home/msmd/.xsession
+echo 'sudo mount -t devpts devpts /dev/pts' >> /home/msmd/.xsession
